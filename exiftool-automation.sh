a259df9352dd164d9e42d0619a1bfb268ef3875c
matheuscezar <matheuscezar@hotmail.com>
2021-03-19 09:21:15 -0300

    Create exiftool-automation.sh

---
+++ exiftool-automation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Downloading all images of URL
+curl -s https://$1 | grep "<img" | grep -v svg | awk -F "src=" '{print $2;}' | cut -d\" -f2 | sort -u > images.tmp
+for url in $(cat images.tmp);
+do
+        wget -q  "$url"
+done
+
+# Executing exiftool of all files
+ls -1 | grep -v images.tmp > files.tmp
+for file in $(cat files.tmp);
+do
+        exiftool "$file" >> output.txt
+        echo -e "================================================================================\n\n" >> output.txt
+done
+cat output.txt
