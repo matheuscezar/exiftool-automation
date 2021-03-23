3f0e31b0d19d2fc0755823898d8a63b996bea44a
matheuscezar <matheuscezar@hotmail.com>
2021-03-22 22:40:14 -0300

    Update exiftool-automation.sh

--- exiftool-automation.sh
+++ exiftool-automation.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
 # Downloading all images of URL
-curl -s https://$1 | grep "<img" | grep -v svg | awk -F "src=" '{print $2;}' | cut -d\" -f2 | sort -u > images.tmp
-for url in $(cat images.tmp);
+curl -s https://$1 | grep "<img" | grep -v svg | awk -F "src=" '{print $2;}' | cut -d\" -f2 | sort -u > files.tmp
+curl -s https://$1 | grep "<a"  | awk -F "href=" '{print $2;}' | cut -d\" -f2 | sort -u | grep ".*\(pdf$\|doc$\|docx$\)" >> files.tmp
+for url in $(cat files.tmp);
 do
         wget -q  "$url"
 done
 
-# Downloading other files
-curl -s https://$1 | grep "<a"  | awk -F "href=" '{print $2;}' | cut -d\" -f2 | sort -u | grep ".*\(pdf$\|doc$\|docx$\)" > others.tmp
-for url in $(cat others.tmp);
-do
-        wget -q "$url"
-done
-
 # Executing exiftool of all files
-ls -1 | grep -v images.tmp > files.tmp
-for file in $(cat files.tmp);
+for file in $(ls -1 |grep -v .tmp | grep -v .sh | grep -v output.txt ) ;
 do
         exiftool "$file" >> output.txt
         echo -e "================================================================================\n\n" >> output.txt
 done
 cat output.txt
+                
