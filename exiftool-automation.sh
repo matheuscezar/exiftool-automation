c01c78fedfcd226a29209d80cc39798811a4ab70
matheuscezar <matheuscezar@hotmail.com>
2021-03-23 08:56:56 -0300

    Update exiftool-automation.sh

--- exiftool-automation.sh
+++ exiftool-automation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Downloading all images of URL
+# Downloading all files of URL
 curl -s https://$1 | grep "<img" | grep -v svg | awk -F "src=" '{print $2;}' | cut -d\" -f2 | sort -u > files.tmp
 curl -s https://$1 | grep "<a"  | awk -F "href=" '{print $2;}' | cut -d\" -f2 | sort -u | grep ".*\(pdf$\|doc$\|docx$\)" >> files.tmp
 for url in $(cat files.tmp);
